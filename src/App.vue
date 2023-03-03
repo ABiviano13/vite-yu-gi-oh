cb69a172bb49f715776ce0e4eeeaba1d984b1d14
ABiviano13 <abiviano0@outlook.com>
2023-03-03 15:42:24 +0100

    FEAT HEADER

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  },
+
+  data(){
+
+  }
+}
 </script>
 
 <template>
-    
+  
+  <AppHeader />
+  <AppMain />
+ 
 </template>
 
-<style scoped>
+<style lang="scss">
+@use './style/general.scss';
 
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script>
+
+export default {
+    data() {
+        return {
+            title: 'Yu-Gi-Oh Api'
+        }
+
+    }
+}
+
+</script>
+
+<template>
+
+    <header class="main-header">
+
+        <div class="container">
+            <img src="" alt="">
+            <h1>
+                {{ title }}
+            </h1>
+        </div>
+
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+.main-header{
+    padding: 30px 36px;
+}
+
+</style>
