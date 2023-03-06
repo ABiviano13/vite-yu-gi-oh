42987da3bb04e4aee20cd8d40651ad6a3a94b1bc
ABiviano13 <abiviano0@outlook.com>
2023-03-06 15:34:50 +0100

    FEAT START componets filters

---
+++ src/components/Filters.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+            search: ''
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <input 
+    type="text" 
+    v-model="search"
+    placeholder="Filtra per nome"
+    >
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
